9e834c1633a1be519f1b9a5a7a9829491a9171c2
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2025-06-20 11:55:51 +0000

    fix: DashBoard.css

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Dashboard.css";
+import "../styles/DashBoard.css";
 import NDVIChart from "../components/NDVICharts.jsx";
 import FieldMap from "../components/FieldMap";
 import NDVILegend from "../components/NDVILegend";
