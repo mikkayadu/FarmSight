01115afc1231377cb9f1e6a9dad8654dca23fff7
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2025-06-21 11:14:20 +0000

    vite-config-1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   optimizeDeps: {
     include: [
       'react-chartjs-2',
       'chart.js',
       'react-leaflet',
       'leaflet'
-    ]
+    ],
   },
   build: {
     commonjsOptions: {
