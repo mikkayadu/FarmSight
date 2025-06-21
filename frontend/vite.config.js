a71e99bf25895a10131f415660fcd9e5ba5aed22
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2025-06-21 11:41:29 +0000

    vite-config-2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), commonjs()],
+  resolve: {
+    alias: {
+      'react-leaflet': path.resolve(__dirname, 'node_modules/react-leaflet'),
+      'leaflet': path.resolve(__dirname, 'node_modules/leaflet'),
+    },
+  },
   optimizeDeps: {
-    include: [
-      'react-chartjs-2',
-      'chart.js',
-      'react-leaflet',
-      'leaflet'
-    ],
+    include: ['react-leaflet', 'leaflet'],
   },
   build: {
     commonjsOptions: {
