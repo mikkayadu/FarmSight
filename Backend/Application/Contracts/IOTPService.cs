3f1320582e017cc3abfe497f2d6845a370551ddb
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-24 13:43:25 +0000

    feat(backend) : Add interfaces

---
+++ Backend/Application/Contracts/IOTPService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Application.Contracts
+{
+    public interface IOTPService
+    {
+        public byte[] GenerateSecretKey(int keySize = 20);
+
+        public string GenerateOtp(byte[] secretKey);
+
+        public bool ValidateOtp(byte[] secretKey, string providedOtp);
+
+        public  Task SendOtpEmailAsync(string email, byte[] secretKey);
+    }
+}
