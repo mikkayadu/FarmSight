576984241be99053aa08bd99530d33324e079960
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-24 13:38:46 +0000

    feat(backend) : Add PhotoService and Accessor

---
+++ Backend/Application/Contracts/Photos/IPhotoAccessor.cs
@@ -0,0 +1,17 @@
+﻿using Application.Models.Photos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Photos
+{
+    public interface IPhotoAccessor
+    {
+        Task<PhotoUploadResult> AddPhoto(IFormFile file);
+        Task<string> DeletePhoto(string publicId);
+        Task<PhotoUploadResult> GetPhoto(string publicId);  
+    }
+}
---
+++ Backend/Application/Contracts/Photos/IUserPhotoService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Models.ApiResult;
+using Application.Models.Photos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Photos
+{
+    public interface IUserPhotoService
+    {
+        Task<Result> AddUserPhotoAsync(string userId, IFormFile file);
+        Task<Result?> DeleteUserPhotoAsync(string userId);
+        Task<Result> GetUserPhotoAsync(string userId);   
+    }
+}
