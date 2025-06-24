8cc2004a779bd9cfff4123059c88b630a1c7e4f9
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-24 13:44:49 +0000

    faeat : Add Services to IdentityProject

---
+++ Backend/Identity/FarmSightIdentityDbContextFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using DotNetEnv;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity
+{
+    public class FarmSightIdentityDbContextFactory : IDesignTimeDbContextFactory<FarmSightIdentityDbContext>
+    {
+        public FarmSightIdentityDbContext CreateDbContext(string[] args)
+        {
+            var apiProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "../FarmSight");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(apiProjectPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<FarmSightIdentityDbContext>();
+
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            if (env == "Development")
+            {
+                Env.Load("../.env.local");
+
+            }
+            else
+            {
+                Env.Load("../.env");
+
+            }
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING_AUTH");
+            builder.UseNpgsql(connectionString);
+
+            return new FarmSightIdentityDbContext(builder.Options);
+        }
+    }
+}
+
