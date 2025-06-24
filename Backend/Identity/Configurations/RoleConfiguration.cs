d52d5e36157d03dceccf46bc8a136829665adfba
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2025-06-24 13:44:00 +0000

    feat : Add Configurations

---
+++ Backend/Identity/Configurations/RoleConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Id = "b760fe05-a63f-43d0-9267-5a46c95kdsie",
+                    Name = "Farmer",
+                    NormalizedName = "Student".ToUpper(),
+                },
+
+                new IdentityRole
+                {
+                    Id = "1d32w46b-c7e5-4a43-be1a-eb2e88f09f82",
+                    Name = "Administrator",
+                    NormalizedName = "Administrator".ToUpper()
+                }
+            );
+        }
+    }
+}
---
+++ Backend/Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "b760fe05-a63f-43d0-9267-5a46c95kdsie",
+                    UserId = "7a223968-23b4-4652-z7b7-8574d048cdb9"
+                },
+
+                new IdentityUserRole<string>
+                {
+                    RoleId = "1d32w46b-c7e5-4a43-be1a-eb2e88f09f82",
+                    UserId = "88k76378-681a-4044-97ed-4ab612217206"
+                }
+                );
+        }
+    }
+}
