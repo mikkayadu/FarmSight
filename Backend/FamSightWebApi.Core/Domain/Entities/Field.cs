995b864eef48184248b4bff509bb2c89acf2f2ca
sedfarouk <sadikalhanssah@gmail.com>
2025-06-26 10:48:00 +0000

    feat: change from polygon coordinates to 4 coordinates

--- Backend/FamSightWebApi.Core/Domain/Entities/Field.cs
+++ Backend/FamSightWebApi.Core/Domain/Entities/Field.cs
@@ -16,8 +16,17 @@ namespace FamSightWebApi.Core.Domain.Entities
 
         public string Name { get; set; }
         public string CropType { get; set; }
-        public Polygon Geometry { get; set; }
-        public NetTopologySuite.Geometries.Point Location { get; set; } // Centroid or anchor point
+        public double Lat1 { get; set; }
+        public double Lng1 { get; set; }
+
+        public double Lat2 { get; set; }
+        public double Lng2 { get; set; }
+
+        public double Lat3 { get; set; }
+        public double Lng3 { get; set; }
+
+        public double Lat4 { get; set; }
+        public double Lng4 { get; set; }
         public double AreaHa { get; set; }
         public DateTime? StartDate { get; set; }
         public string Status { get; set; }
