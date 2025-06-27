deb73c0e1e6f7259edcda88d7bf4e3e188418347
sedfarouk <sadikalhanssah@gmail.com>
2025-06-27 00:18:01 +0000

    feat: define interface and repository for EOData

---
+++ Backend/FarmSightWebApi.ApplicationCore/Domain/RepositoryContracts/IEODataRepository.cs
@@ -0,0 +1,17 @@
+﻿using FarmSightWebApi.ApplicationCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FarmSightWebApi.ApplicationCore.Domain.RepositoryContracts
+{
+    public interface IEODataRepository
+    {
+        Task<EOData> AddAsync(EOData data);
+        Task<EOData> GetByIdAsync(Guid id);
+        Task<IEnumerable<EOData>> GetByFieldIdAsync(Guid fieldId);
+        Task<IEnumerable<EOData>> GetAllAsync();
+        Task UpdateAsync(EOData data);
+        Task DeleteAsync(EOData data);
+    }
+}
